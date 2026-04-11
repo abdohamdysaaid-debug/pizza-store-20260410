556c8ef70bc52d977b5ef6524d214ec287d26a80
Abdo ElHDAD <Abdohamdysaaid@gmail.com>
2026-04-11 04:19:05 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', 
+  base: '/pizza-store-20260410/',
+  plugins: [react()],
 })
